f553cdfe57e7b864b15211fe2cf4a7097d47ff44
somnathasati <somnathasati@gmail.com>
2013-10-20 12:43:56 +0530

    decode poly

--- Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
+++ Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 import android.app.Dialog;
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class GMapV2Direction {
 	
@@ -140,6 +141,8 @@ public class GMapV2Direction {
 	    return -1;
 	}
 	
+	
+	
 	public ArrayList<LatLng> getDirection (Document doc) {
 	    NodeList nl1, nl2, nl3;
 	    ArrayList<LatLng> listGeopoints = new ArrayList<LatLng>();
@@ -178,10 +181,37 @@ public class GMapV2Direction {
 	    return listGeopoints;
 	}
 
-	private ArrayList<LatLng> decodePoly(String textContent) {
+	private ArrayList<LatLng> decodePoly(String encoded) {
 		// TODO Auto-generated method stub
-		return null;
+		 ArrayList<LatLng> poly = new ArrayList<LatLng>();
+		    int index = 0, len = encoded.length();
+		    int lat = 0, lng = 0;
+		    while (index < len) {
+		        int b, shift = 0, result = 0;
+		        do {
+		            b = encoded.charAt(index++) - 63;
+		            result |= (b & 0x1f) << shift;
+		            shift += 5;
+		        } while (b >= 0x20);
+		        int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+		        lat += dlat;
+		        shift = 0;
+		        result = 0;
+		        do {
+		            b = encoded.charAt(index++) - 63;
+		            result |= (b & 0x1f) << shift;
+		            shift += 5;
+		        } while (b >= 0x20);
+		        int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+		        lng += dlng;
+
+		        LatLng position = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+		        poly.add(position);
+		    }
+		    return poly;
 	}
+	
+	
 	
 }
 
