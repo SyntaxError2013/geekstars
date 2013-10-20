e8244dbf0782a018ccc7b30775290333b1da3383
somnathasati <somnathasati@gmail.com>
2013-10-20 12:25:48 +0530

    direction from nodes

--- Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
+++ Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
@@ -14,6 +14,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -105,10 +107,7 @@ public class GMapV2Direction {
 	}
 
 	}
-	private ArrayList<LatLng> getDirection(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 	private Document getDocument(LatLng start, LatLng end,
 			String mode) {
@@ -133,6 +132,56 @@ public class GMapV2Direction {
 		    return null;
 	}
 	
+	private int getNodeIndex(NodeList nl, String nodename) {
+	    for(int i = 0 ; i < nl.getLength() ; i++) {
+	        if(nl.item(i).getNodeName().equals(nodename))
+	            return i;
+	    }
+	    return -1;
+	}
+	
+	public ArrayList<LatLng> getDirection (Document doc) {
+	    NodeList nl1, nl2, nl3;
+	    ArrayList<LatLng> listGeopoints = new ArrayList<LatLng>();
+	    nl1 = doc.getElementsByTagName("step");
+	    if (nl1.getLength() > 0) {
+	        for (int i = 0; i < nl1.getLength(); i++) {
+	            Node node1 = nl1.item(i);
+	            nl2 = node1.getChildNodes();
+
+	            Node locationNode = nl2.item(getNodeIndex(nl2, "start_location"));
+	            nl3 = locationNode.getChildNodes();
+	            Node latNode = nl3.item(getNodeIndex(nl3, "lat"));
+	            double lat = Double.parseDouble(latNode.getTextContent());
+	            Node lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+	            double lng = Double.parseDouble(lngNode.getTextContent());
+	            listGeopoints.add(new LatLng(lat, lng));
+
+	            locationNode = nl2.item(getNodeIndex(nl2, "polyline"));
+	            nl3 = locationNode.getChildNodes();
+	            latNode = nl3.item(getNodeIndex(nl3, "points"));
+	            ArrayList<LatLng> arr = decodePoly(latNode.getTextContent());
+	            for(int j = 0 ; j < arr.size() ; j++) {
+	                listGeopoints.add(new LatLng(arr.get(j).latitude, arr.get(j).longitude));
+	            }
+
+	            locationNode = nl2.item(getNodeIndex(nl2, "end_location"));
+	            nl3 = locationNode.getChildNodes();
+	            latNode = nl3.item(getNodeIndex(nl3, "lat"));
+	            lat = Double.parseDouble(latNode.getTextContent());
+	            lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+	            lng = Double.parseDouble(lngNode.getTextContent());
+	            listGeopoints.add(new LatLng(lat, lng));
+	        }
+	    }
+
+	    return listGeopoints;
+	}
+
+	private ArrayList<LatLng> decodePoly(String textContent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
 
