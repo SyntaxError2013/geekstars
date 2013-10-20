d60928866209999c00f6c7fe2d7da1e6e1183426
somnathasati <somnathasati@gmail.com>
2013-10-20 12:20:45 +0530

    get document from httpclient

--- Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
+++ Jogging/src/in/syntaxerror/jogging/GMapV2Direction.java
@@ -1,8 +1,18 @@
 package in.syntaxerror.jogging;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.w3c.dom.Document;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -11,8 +21,18 @@ import android.app.Dialog;
 import android.os.AsyncTask;
 
 public class GMapV2Direction {
+	
+	public GMapV2Direction() 
+	{
+		
+	    }
 
-	public class GetDirectionsAsyncTask extends AsyncTask<Map<String, String>, Object, ArrayList<LatLng>> {
+	public class GetDirectionsAsyncTask extends AsyncTask<Map<String, String>, Object, ArrayList<LatLng>> 
+	
+	
+	{
+		
+		
 	public final static String MODE_DRIVING = "driving";
 	public final static String MODE_WALKING = "walking";
 
@@ -28,10 +48,7 @@ public class GMapV2Direction {
 	private Dialog progressDialog;
 	
 	
-	public GMapV2Direction() 
-	{
-		
-	    }
+	
 	
 	public GetDirectionsAsyncTask(MapActivity activity ) 
 	{
@@ -93,10 +110,27 @@ public class GMapV2Direction {
 		return null;
 	}
 
-	private Document getDocument(LatLng fromPosition, LatLng toPosition,
-			String string) {
+	private Document getDocument(LatLng start, LatLng end,
+			String mode) {
 		// TODO Auto-generated method stub
-		return null;
+		 String url = "http://maps.googleapis.com/maps/api/directions/xml?" 
+		            + "origin=" + start.latitude + "," + start.longitude  
+		            + "&destination=" + end.latitude + "," + end.longitude 
+		            + "&sensor=false&units=metric&mode="+ mode;
+
+		    try {
+		        HttpClient httpClient = new DefaultHttpClient();
+		        HttpContext localContext = new BasicHttpContext();
+		        HttpPost httpPost = new HttpPost(url);
+		        HttpResponse response = httpClient.execute(httpPost, localContext);
+		        InputStream in = response.getEntity().getContent();
+		        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		        Document doc = builder.parse(in);
+		        return doc;
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+		    return null;
 	}
 	
 	
