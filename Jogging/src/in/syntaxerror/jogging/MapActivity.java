795ed96aad19fb3919b968b394c41f26362d2272
somnathasati <somnathasati@gmail.com>
2013-10-20 14:22:03 +0530

    semifinal mapactivity

--- Jogging/src/in/syntaxerror/jogging/MapActivity.java
+++ Jogging/src/in/syntaxerror/jogging/MapActivity.java
@@ -4,8 +4,10 @@ import in.syntaxerror.jogging.GMapV2Direction.GetDirectionsAsyncTask;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import android.speech.tts.TextToSpeech;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -13,12 +15,17 @@ import java.util.TimerTask;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,17 +38,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements TextToSpeech.OnInitListener {
 
-	
+	protected static final int REQUEST_CODE = 0;
 	private double weight;
 	private GoogleMap googleMap;
+	private TextToSpeech tts;
 	private GPSTracker gps;
+	 double	d1,speed, calories;
 	private GMapV2Direction md1 = new GMapV2Direction();
 	private Date start,end;
 	private ArrayList<Double> lat = new ArrayList<Double>();
 	private ArrayList<Double>  lng = new ArrayList<Double>();
 	private ArrayList<Double> dis = new ArrayList<Double>();
+	double latitude,longitude;
 	private Timer timer;
 	private TimerTask doAsynchronousTask;
     private final Handler handler = new Handler();
@@ -97,7 +107,6 @@ public class MapActivity extends Activity {
         LocateDirections(29.869506,77.894973,
         		29.863651,77.895522, GMapV2Direction.MODE_DRIVING );;
         
-
 		 gps = new GPSTracker(MapActivity.this);
 		// check if GPS enabled     
         if(gps.canGetLocation()){
@@ -251,5 +260,100 @@ public class MapActivity extends Activity {
 		        rectLine.add(directionPoints.get(i));
 		    }
 		    newPolyline = googleMap.addPolyline(rectLine);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	   
+
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+        case R.id.stop:
+        	onPause();
+        	tts = new TextToSpeech(this, this);
+        	speakOut();
+       
+       Calendar c = Calendar.getInstance(); 
+   		 end=c.getTime(); 	 
+   		long millis = end.getTime() - start.getTime();
+   		double Hours = millis/(1000 * 60 * 60);
+   	 double t1 = (millis % (1000*60*60));
+       for(int i=0; i<dis.size(); i++)
+       {
+    	   d1=d1+dis.get(i);
+       }
+       speed=d1/t1;
+       calories= Hours*(weight*((3.5+speed*0.2)/3.5));
+        	
+        	new AlertDialog.Builder(this)
+			.setTitle("Are you done with tracking to exit?")
+			.setMessage("your distance travelled(in meters), mileage(in meter/minute) and calories burnt are "+ d1+", "+speed+", "+calories)
+			.setPositiveButton("Yes",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// continue with delete
+								android.os.Process.killProcess(android.os.Process.myPid());
+							}
+						})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						// do nothing
+                  onResume();
+					}
+				}).show();
+    
+            return true;
+       
+        default:
+            return super.onOptionsItemSelected(item);
+    }
+	}
+	
+	 public void onDestroy() {
+	        // Don't forget to shutdown tts!
+	        if (tts != null) {
+	            tts.stop();
+	            tts.shutdown();
+	        }
+	        super.onDestroy();
+	    }
+	@Override
+	public void onInit(int status) {
+		// TODO Auto-generated method stub
+		
+				if (status == TextToSpeech.SUCCESS) {
+					 
+		            int result = tts.setLanguage(Locale.US);
+		 
+		            if (result == TextToSpeech.LANG_MISSING_DATA
+		                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+		                Log.e("TTS", "This Language is not supported");
+		            } else {
+		                
+		                speakOut();
+		            }
+		 
+		        } else {
+		            Log.e("TTS", "Initilization Failed!");
+		        }
+		
+	}
+	
+	private void speakOut() {
+		// TODO Auto-generated method stub
+		String a=Double.toString(d1);
+		String b=Double.toString(speed);
+		String c=Double.toString(calories);
+		String s = "your distance travelled mileage and calories burnt are respectively "+a+" meters "+b+" meters per minute "+c+" calories";
+		tts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
 	}
 }
